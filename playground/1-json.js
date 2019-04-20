2d55c04d2760ecb734c43877a6332de794af43d6
Jeffery Cosby <jcoz1968@gmail.com>
2019-04-20 16:59:08 -0500

    json playground finish up

--- playground/1-json.js
+++ playground/1-json.js
@@ -1,20 +1,5 @@
 const fs = require('fs');
 
-// const book = {
-//   title: 'Ego is the Enemy',
-//   author: 'Ryan Holiday'
-// };
-
-// const bookJSON = JSON.stringify(book);
-// const parsedData = JSON.parse(bookJSON);
-
-// fs.writeFileSync('1-json.json', bookJSON);
-
-// const dataBuffer = fs.readFileSync('1-json.json');
-// const dataJson = dataBuffer.toString();
-// const data = JSON.parse(dataJson);
-// console.log(data.title);
-
 const dataBuffer = fs.readFileSync('1-json.json');
 const data = JSON.parse(dataBuffer.toString());
 
