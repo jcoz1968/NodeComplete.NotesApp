0a906d73a4614e9389de30708c4a7c91b330e19e
Jeffery Cosby <jcoz1968@gmail.com>
2019-04-20 16:34:45 -0500

    yargs commands

--- app.js
+++ app.js
@@ -1,10 +1,40 @@
 const chalk = require('chalk');
+const yargs = require('yargs');
 const getNotes = require('./notes');
 
-const command = process.argv[2];
+yargs.version('1.1.0');
 
-if (command === 'add') {
-  console.log('Adding note');
-} else if (command === 'remove') {
-  console.log('Removing note');
-}
+yargs.command({
+  command: 'add',
+  describe: 'Add a new note',
+  handler: function () {
+    console.log('Adding a new note');
+  }
+});
+
+yargs.command({
+  command: 'remove',
+  describe: 'Remove a note',
+  handler: function () {
+    console.log('Removing note');
+  }
+});
+
+yargs.command({
+  command: 'list',
+  describe: 'list notes',
+  handler: function () {
+    console.log('List notes');
+  }
+});
+
+yargs.command({
+  command: 'read',
+  describe: 'read a note',
+  handler: function () {
+    console.log('Read note');
+  }
+});
+
+console.log(process.argv);
+console.log(yargs.argv);
