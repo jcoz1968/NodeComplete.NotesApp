d776d2241e2dc30d66342938f032462a4ce9a46e
Jeffery Cosby <jcoz1968@gmail.com>
2019-04-20 16:43:48 -0500

    more yargs commands

--- app.js
+++ app.js
@@ -7,8 +7,21 @@ yargs.version('1.1.0');
 yargs.command({
   command: 'add',
   describe: 'Add a new note',
-  handler: function () {
-    console.log('Adding a new note');
+  builder: {
+    title: {
+      describe: 'Note title',
+      demandOption: true,
+      type: 'string'
+    },
+    body: {
+      describe: 'Note body',
+      demandOption: true,
+      type: 'string'      
+    }
+  },
+  handler: function (argv) {
+    console.log('title: ' + argv.title);
+    console.log('body: ' + argv.body);
   }
 });
 
@@ -36,5 +49,6 @@ yargs.command({
   }
 });
 
-console.log(process.argv);
-console.log(yargs.argv);
+// console.log(process.argv);
+// console.log(yargs.argv);
+yargs.parse();
