2b06a85abebb9f05e35209e84aecaaee67646d3b
Jeffery Cosby <jcoz1968@gmail.com>
2019-04-20 17:30:02 -0500

    remove note file

--- notes.js
+++ notes.js
@@ -1,6 +1,7 @@
 
 
 const fs = require('fs');
+const chalk = require('chalk');
 
 const getNotes = function () {
   return 'Your notes...';
@@ -23,6 +24,19 @@ const addNote = function (title, body) {
   }
 }
 
+const removeNote = function (title) {
+  const notes = loadNotes();
+  const notesToKeep = notes.filter(function (note) { 
+    return note.title !== title
+  });
+  if (notes.length > notesToKeep.length) {
+    console.log(chalk.green.inverse('Note removed!'));
+    saveNotes(notesToKeep);
+  } else {
+    console.log(chalk.red.inverse('No note found!'));
+  }
+}
+
 const saveNotes = function (notes) {
   const dataJson = JSON.stringify(notes);
   fs.writeFileSync('notes.json', dataJson);
@@ -40,5 +54,6 @@ const loadNotes = function () {
 
 module.exports = {
   getNotes: getNotes,
-  addNote: addNote
+  addNote: addNote,
+  removeNote: removeNote
 }
