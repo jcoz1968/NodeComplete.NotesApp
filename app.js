8148cd577b3d30d42e5d82f61ecac553b00fd17a
Jeffery Cosby <jcoz1968@gmail.com>
2019-04-20 16:09:13 -0500

    chalk

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const validator = require('validator');
+const chalk = require('chalk');
 const getNotes = require('./notes');
 
 const notesMessage = getNotes();
@@ -7,5 +8,14 @@ console.log(notesMessage);
 
 // console.log(validator.isEmail('coz@nodehero.com'));
 console.log(validator.isURL('https://www.npmjs.com'));
+
+console.log(chalk.green('Success!'));
+console.log(chalk.blue('Blue Success!'));
+console.log(chalk.red('Red Success!'));
+
+console.log(chalk.bgGreen('BG Success!'));
+console.log(chalk.bgBlue('BG Blue Success!'));
+console.log(chalk.bgRed('BG Red Success!'));
+
 
 
