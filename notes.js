1a9fb8d7dd04b299e3a9a06cc4c259368e2870ff
Jeffery Cosby <jcoz1968@gmail.com>
2019-04-20 17:15:31 -0500

    add note command to file

--- notes.js
+++ notes.js
@@ -1,6 +1,44 @@
 
+
+const fs = require('fs');
+
 const getNotes = function () {
   return 'Your notes...';
 }
 
-module.exports = getNotes;
+const addNote = function (title, body) {
+  const notes = loadNotes();
+  const duplicateNotes = notes.filter(function (note) { 
+    return note.title == title;
+  });
+  if (duplicateNotes.length === 0) {
+    notes.push({
+      title: title,
+      body: body
+    });    
+    saveNotes(notes);
+    console.log('new note added...')
+  } else {
+    console.log('note already exists...')
+  }
+}
+
+const saveNotes = function (notes) {
+  const dataJson = JSON.stringify(notes);
+  fs.writeFileSync('notes.json', dataJson);
+}
+
+const loadNotes = function () {
+  try {
+    const buffer = fs.readFileSync('notes.json');
+    const dataJson = buffer.toString();
+    return JSON.parse(dataJson);
+  } catch (e) {
+    return [];
+  }
+}
+
+module.exports = {
+  getNotes: getNotes,
+  addNote: addNote
+}
