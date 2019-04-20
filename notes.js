6c16aecb323015055b2bd1ce3fcd446ce1746ac1
Jeffery Cosby <jcoz1968@gmail.com>
2019-04-20 18:42:37 -0500

    debugger

--- notes.js
+++ notes.js
@@ -9,6 +9,7 @@ const getNotes = function () {
 
 const addNote = function (title, body) {
   const notes = loadNotes();
+
   const duplicateNotes = notes.filter(function (note) { 
     return note.title == title;
   });
