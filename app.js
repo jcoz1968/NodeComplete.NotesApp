e1df54a6b6490668accbcaefd7c82657f0838344
Jeffery Cosby <jcoz1968@gmail.com>
2019-04-20 16:02:56 -0500

    npm modules

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
+const validator = require('validator');
 const getNotes = require('./notes');
 
 const notesMessage = getNotes();
+
 console.log(notesMessage);
+
+// console.log(validator.isEmail('coz@nodehero.com'));
+console.log(validator.isURL('https://www.npmjs.com'));
+
+
